4636d3a658e79bb4298f1166386381e1f19484c3
Joachim Køcher Kelsen <71125597+jokk-itu@users.noreply.github.com>
2023-10-22 15:57:30 +0200

    Initial commit

---
+++ Api/Program.cs
@@ -0,0 +1,41 @@
+using Serilog;
+
+var builder = WebApplication.CreateBuilder(args);
+var configuration = builder.Configuration;
+var services = builder.Services;
+
+services.AddControllers();
+services.AddEndpointsApiExplorer();
+services.AddSwaggerGen();
+
+var serilogConfiguration = configuration.GetSection("Serilog");
+Log.Logger = new LoggerConfiguration()
+  .Enrich.FromLogContext()
+  .Enrich.WithProcessName()
+  .Enrich.WithProcessId()
+  .Enrich.WithThreadName()
+  .Enrich.WithThreadId()
+  .Enrich.WithMemoryUsage()
+  .Enrich.WithProperty("ContainerId", Environment.GetEnvironmentVariable("HOSTNAME"))
+  .WriteTo.Console()
+  .WriteTo.Seq(serilogConfiguration.GetSection("Seq").GetValue<string>("Url"))
+  .CreateBootstrapLogger();
+
+try
+{
+  var app = builder.Build();
+  app.UseSwagger();
+  app.UseSwaggerUI();
+  app.UseSerilogRequestLogging();
+  app.UseAuthorization();
+  app.MapControllers();
+  app.Run();
+}
+catch(Exception e)
+{
+  Log.Error(e, "Unexpected error occurred");
+}
+finally
+{
+  await Log.CloseAndFlushAsync();
+}
