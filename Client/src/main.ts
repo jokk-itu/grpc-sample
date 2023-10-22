363d6021d00fef5b012535d22695fb850fc8ad6b
DESKTOP-QJH6G9M\joach <joachim@kelsen.nu>
2023-10-22 22:28:56 +0200

    add: dkfds styling package to app

--- Client/src/main.ts
+++ Client/src/main.ts
@@ -1,7 +1,8 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
+import * as dkfds from 'dkfds'; // fake error
 import { AppModule } from './app/app.module';
-
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
+dkfds.init();
